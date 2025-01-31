029c790d39034fec7b88746f0688cf302058e7e0
quantumde1 <meserakovarsenij810@gmail.com>
2025-01-31 23:10:53 +0300

    fixed camera when switching to 2.5d

--- source/character.c
+++ source/character.c
@@ -1,52 +1,40 @@
 #include "raylib.h"
+#include <math.h>
 
 void DrawCharacter(Vector3 initialPosition, char* modelPath) {
     DrawCube(initialPosition, 2.0f, 2.0f, 2.0f, RED);
 }
 
 void MoveCharacter3D(Vector3 *position, Camera3D *camera, short isNights) {
+    static float currentSpeed = 0.0f;
+    const float maxSpeed = 0.26f;
+    const float acceleration = 0.013f;
+    const float deceleration = 0.013f;
     float movementSpeed = 0.2f;
-    /*
-     * -1 - a pressed
-     * 0 - nothing pressed
-     * 1 - d pressed
-     */
-    int vertical = 0;
-    /*
-     * -1 - w pressed
-     * 0 - nothing pressed
-     * 1 - s pressed
-     */
-    int horizontal = 0;
-    if (horizontal != 0 && vertical != 0) {
-        movementSpeed *= 0.7f;
-    }
-    if (IsKeyDown(KEY_W)) {
-        horizontal = -1;
-    }
-    else if (IsKeyDown(KEY_S)) {
-        horizontal = 1;
-    }
-    if (IsKeyDown(KEY_A)) {
-        vertical = -1;
-    }
-    else if (IsKeyDown(KEY_D)) {
-        vertical = 1;
-    }
+
+    int vertical = IsKeyDown(KEY_D) - IsKeyDown(KEY_A);
+    int horizontal = IsKeyDown(KEY_S) - IsKeyDown(KEY_W);
+
     if (isNights == 0) {
-        position->z += movementSpeed*horizontal;
-        position->x += movementSpeed*vertical;
-        camera->position.z += movementSpeed*horizontal;
-        camera->position.x += movementSpeed*vertical;
-        camera->target = (Vector3){ position->x, position->y, position->z };
+        if (horizontal != 0 && vertical != 0) {
+            movementSpeed *= 0.7f;
+        }
+        position->z += movementSpeed * horizontal;
+        position->x += movementSpeed * vertical;
+        camera->position.z += movementSpeed * horizontal;
+        camera->position.x += movementSpeed * vertical;
+    } else {
+        if (horizontal != 0 || vertical != 0) {
+            currentSpeed = fminf(currentSpeed + acceleration, maxSpeed);
+        } else {
+            currentSpeed = fmaxf(currentSpeed - deceleration, 0.0f);
+        }
+        position->y += currentSpeed * -horizontal;
+        position->x += currentSpeed * vertical;
+        camera->position.y += position->y;
+        camera->position.x += currentSpeed * vertical;
     }
-    if (isNights == 1) {
-        position->y += movementSpeed*-(horizontal);
-        position->x += movementSpeed*vertical;
-        camera->position.y += movementSpeed*-(horizontal);
-        camera->position.x += movementSpeed*vertical;
-        camera->target = (Vector3){ position->x, position->y, position->z };
-    }
+    camera->target = *position;
 }
 
 void JumpCharacter3D(Vector3 *position, Camera3D *camera) {
